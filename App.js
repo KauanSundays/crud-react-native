b846a3b46185a1bfe4bf257a700b39c604044c77
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-07-22 09:29:44 -0300

    feat: "WIP"

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Task from './src/pages/newTask';
 
+const Stack = createStackNavigator()
 export default function App() {
   return (
     <NavigationContainer>
@@ -11,7 +13,7 @@ export default function App() {
         <Stack.Screen
           name="Task"
           component={Task}
-          
+
           />
       </Stack.Navigator>
     </NavigationContainer>
