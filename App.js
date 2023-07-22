5f3093ffb539d5a9d95fc76e21f31fd8d965ec2a
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-07-22 09:23:04 -0300

    feat: "WIP"

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Task">
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/pages/newTask/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {View, Text} from "react-native"
+
+
+export default function Task() {
+    return (
+        <View>
+            <Text>
+                Page Tasks
+            </Text>
+        </View>
+    )
+}
