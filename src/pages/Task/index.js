a4b3789a6156be9420cce142c482723591fb60e5
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-07-25 16:37:14 -0300

    feat: "WIP"

--- src/pages/Task/index.js
+++ src/pages/Task/index.js
@@ -7,15 +7,29 @@ import {
     FlatList
 } from "react-native"
 
+import database from "../../config/firebaseconfig"
+import { FontAwesome } from "@expo/vector-icons"
+//import styles from "../../config/firebaseconfig"
 
-export default function Task() {
+export default function Task({ navigation }) {
     const [task, setTask] = useState([])
 
+    useEffect(() =>{
+        database.collection("Tasks").onSnapshot((query)=>{
+            const list = []
+            query.forEach((doc)=>{
+                list.push({...doc.data(), id: doc.id })
+            })
+            setTask(list)
+        })
+    }, [])
     return (
-        <View>
-            <Text>
-                Page Tasks
-            </Text>
+        <View style={styles.container}>
+            <FlatList/>
+            <TouchableOpacity>
+                <Text style={styles.iconButton}>+</Text>
+            </TouchableOpacity>
+
         </View>
     )
 }
