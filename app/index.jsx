09fe2abd1494d578a97e3b676419eb22ece9f7bb
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-10-11 22:00:29 -0300

    feat: wip

--- app/index.jsx
+++ app/index.jsx
@@ -15,13 +15,51 @@ const app = () => {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 100,
+    gap: 40
+  },
+  form: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: "100%",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '65%',
+    height: 40,
+    marginVertical: 10,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+  },
+  buttons: {
+    width: '65%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  button: {
+    backgroundColor: '#007AFF',
+    justifyContent: "center",
+    borderRadius: 5,
+    alignItems: 'center',
+    width: "35%",
+    aspectRatio: 2.10
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  }
+});
 
-    button: {
-        backgroundColor:'#000',
-    }
-})
 
 export default app
